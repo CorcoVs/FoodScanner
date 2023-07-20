cd7d5f37888b863b566dd164ffbed9d773ae2fd0
Andrei Itt <55024624+CorcoVs@users.noreply.github.com>
2023-07-20 18:21:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const scanner = document.getElementById("scanner");
+
+// Function to handle the barcode scanning
+function handleScan(decodedData) {
+  // Replace #### in the fetch URL with the scanned barcode
+  const barcode = decodedData;
+  const apiUrl = `https://world.openfoodfacts.net/api/v2/product/${barcode}`;
+
+  // Fetch product data using the scanned barcode
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      const productName = json.product.product_name;
+      console.log("Product Name:", productName);
+
+      const nutritionScore = json.product.nutriscore_score;
+      const nutriscoreGrade = json.product.nutriscore_grade;
+      console.log(`Nutriscore ${nutritionScore}, grade ${nutriscoreGrade}`);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+// Function to start the barcode scanner
+function startScanner() {
+  // QuaggaJS configuration
+  const config = {
+    inputStream: {
+      name: "Live",
+      type: "LiveStream",
+      target: video,
+    },
+    decoder: {
+      readers: ["ean_reader"],
+    },
+  };
+
+  // Start the barcode scanner
+  Quagga.init(config, function (err) {
+    if (err) {
+      console.error("Error initializing Quagga:", err);
+      return;
+    }
+    Quagga.start();
+    Quagga.onDetected(function (result) {
+      handleScan(result.codeResult.code);
+      Quagga.stop();
+    });
+  });
+}
+
+// Start scanning when the page loads
+document.addEventListener("DOMContentLoaded", startScanner);
